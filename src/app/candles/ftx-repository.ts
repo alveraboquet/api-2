ad023ed96e6a72b33440e03740d19058b7345f15
jinxcapital <jinxcapital@protonmail.com>
2021-12-03 10:18:14 +0100

    :card_file_box: FTXCandleRepository

---
+++ src/app/candles/ftx-repository.ts
@@ -0,0 +1,25 @@
+import { subHours } from 'date-fns';
+import { Candle } from 'domain/candles';
+import { CandleRepository, GetCandlesOptions } from 'domain/candles/repository';
+import { FTXAPI } from 'infra/ftx/api';
+import { mapFTXResponse } from 'infra/ftx/mapper';
+
+class FTXCandleRepository implements CandleRepository {
+  getCandles = async ({ base, quote }: GetCandlesOptions) => {
+    const endTime = new Date();
+    const startTime = subHours(endTime, 48);
+
+    const data = await FTXAPI.fetchCandles({
+      base,
+      quote,
+      endTime,
+      startTime,
+    });
+
+    const candles = mapFTXResponse(data);
+
+    return Candle.fromArray(candles);
+  };
+}
+
+export default new FTXCandleRepository();
