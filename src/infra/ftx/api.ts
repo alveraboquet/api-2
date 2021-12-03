5ae7c91ba813d56aaff2a274a1152010747e5b3e
jinxcapital <jinxcapital@protonmail.com>
2021-12-03 09:45:25 +0100

    :sparkles: FTXAPI

---
+++ src/infra/ftx/api.ts
@@ -0,0 +1,50 @@
+import fetch from 'node-fetch';
+
+interface FetchCandlesOptions {
+  quote: string;
+  base: string;
+  startTime: Date;
+  endTime: Date;
+}
+
+type Entry = {
+  time: number;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+};
+
+type Response = Array<Entry>;
+
+const fetchCandles = async (
+  options: FetchCandlesOptions,
+): Promise<Response> => {
+  const startTime = Math.round(options.startTime.getTime() / 1000);
+  const endTime = Math.round(options.endTime.getTime() / 1000);
+  const pair = `${options.base}/${options.quote}`.toLowerCase();
+
+  try {
+    const response = await fetch(
+      `https://ftx.com/api/markets/${pair}/candles?resolution=3600&start_time=${startTime}&end_time=${endTime}`,
+    );
+
+    const data = (await response.json()) as {
+      success: boolean;
+      result: Entry[];
+    };
+    if (data.success) {
+      return data.result;
+    }
+  } catch (e) {
+    console.error(`infra/ftx/api error:`, e);
+
+    return [];
+  }
+
+  return [];
+};
+
+export const FTXAPI = {
+  fetchCandles,
+};
