026020f5cafb3020ece1fffb8d41411a15b1b643
jinxcapital <jinxcapital@protonmail.com>
2021-12-04 12:11:50 +0100

    :sparkles: Nvm do explain in detail

--- src/app/http/request-handers/root.ts
+++ src/app/http/request-handers/root.ts
@@ -1,16 +1,37 @@
+import { Exchange } from 'domain/exchanges';
 import { Request, Response } from 'express';
+
+const SUPPORTED_EXCHANGES = [
+  Exchange.Binance,
+  Exchange.Coinbase,
+  Exchange.FTX,
+  Exchange.Kucoin,
+];
 
 export default (_req: Request, res: Response) => {
   res.setHeader('Cache-Control', 'public, max-age=30');
-  res
-    .status(200)
-    .json([
-      [
+  res.status(200).json([
+    {
+      route: '/candles/:pair',
+      params: {
+        pair: {
+          type: 'string',
+          required: true,
+        },
+        exchange: {
+          type: 'string',
+          required: false,
+          default: 'ftx',
+          values: SUPPORTED_EXCHANGES,
+        },
+      },
+      examples: [
         '/candles/btc:usd',
         '/candles/eth:usdt',
         '/candles/bnb:busd?exchange=binance',
         '/candles/mana:usdc?exchange=coinbase',
         '/candles/kcs:usdt?exchange=kucoin',
       ],
-    ]);
+    },
+  ]);
 };
