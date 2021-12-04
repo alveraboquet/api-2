849a673fc53ef29b8302959cf854437c57996307
jinxcapital <jinxcapital@protonmail.com>
2021-12-04 11:24:55 +0100

    :sparkles: Show Coinbase USDC example

--- src/app/http/request-handers/root.ts
+++ src/app/http/request-handers/root.ts
@@ -1,7 +1,7 @@
 import { Exchange } from 'domain/exchanges';
 import { Request, Response } from 'express';
 
-const SUPPORTED_EXCHANGES = [Exchange.Binance, Exchange.FTX];
+const SUPPORTED_EXCHANGES = [Exchange.Binance, Exchange.Coinbase, Exchange.FTX];
 
 export default (_req: Request, res: Response) => {
   res.setHeader('Cache-Control', 'public, max-age=30');
@@ -24,7 +24,7 @@ export default (_req: Request, res: Response) => {
         '/candles/btc:usd',
         '/candles/eth:usdt',
         '/candles/bnb:busd?exchange=binance',
-        '/candles/ltc:usdc?exchange=binance',
+        '/candles/mana:usdc?exchange=coinbase',
       ],
       note: 'Only USD pairs are supported.',
     },
