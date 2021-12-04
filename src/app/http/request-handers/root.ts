eb1b2f4a1aebe9bec23801750169d13894655086
jinxcapital <jinxcapital@protonmail.com>
2021-12-04 11:29:34 +0100

    :boom: Simpler API explanation

--- src/app/http/request-handers/root.ts
+++ src/app/http/request-handers/root.ts
@@ -1,31 +1,15 @@
-import { Exchange } from 'domain/exchanges';
 import { Request, Response } from 'express';
-
-const SUPPORTED_EXCHANGES = [Exchange.Binance, Exchange.Coinbase, Exchange.FTX];
 
 export default (_req: Request, res: Response) => {
   res.setHeader('Cache-Control', 'public, max-age=30');
-  res.status(200).json([
-    {
-      route: '/candles/:pair',
-      params: {
-        pair: {
-          type: 'string',
-          required: true,
-        },
-        exchange: {
-          type: 'string',
-          required: false,
-          default: 'ftx',
-          values: SUPPORTED_EXCHANGES,
-        },
-      },
-      examples: [
+  res
+    .status(200)
+    .json([
+      [
         '/candles/btc:usd',
         '/candles/eth:usdt',
         '/candles/bnb:busd?exchange=binance',
         '/candles/mana:usdc?exchange=coinbase',
       ],
-    },
-  ]);
+    ]);
 };
