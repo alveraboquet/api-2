de29c9b6297c9b0cfbc522e683efa31ac58974a0
jinxcapital <jinxcapital@protonmail.com>
2021-12-04 10:06:14 +0100

    :sparkles: API description

--- src/app/http/request-handers/root.ts
+++ src/app/http/request-handers/root.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from 'express';
 
 export default (_req: Request, res: Response) => {
-  res.status(200).send(':)');
+  res.setHeader('Cache-Control', 'public, max-age=30');
+  res.status(200).json([
+    {
+      route: '/candles/:pair',
+      examples: ['/candles/btc:usd', '/candles/eth:usdt', '/candles/ltc:usdc'],
+    },
+  ]);
 };
