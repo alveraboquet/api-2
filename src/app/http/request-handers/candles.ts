bd5f1bfb8cda38412a0a43cbb756be5f2fe34109
jinxcapital <jinxcapital@protonmail.com>
2021-12-04 10:49:35 +0100

    :sparkles: Multi exchange support

--- src/app/http/request-handers/candles.ts
+++ src/app/http/request-handers/candles.ts
@@ -1,24 +1,42 @@
+import BinanceRepository from 'app/candles/binance-repository';
 import FtxRepository from 'app/candles/ftx-repository';
+import { Exchange } from 'domain/exchanges';
 import { Request, Response } from 'express';
+
+const SUPPORTED_EXCHANGES = [Exchange.Binance, Exchange.FTX];
 
 export default async (req: Request, res: Response) => {
   res.setHeader('Cache-Control', 'public, max-age=0');
 
+  const exchange = (req.query.exchange as Exchange) || Exchange.FTX;
+  if (!SUPPORTED_EXCHANGES.includes(exchange)) {
+    return res.status(400).json({ success: false, meta: {}, data: [] });
+  }
+
   const pair = req.params.pair;
   if (!pair) {
-    return res.status(400).json({ success: false, data: [] });
+    return res.status(400).json({ success: false, meta: {}, data: [] });
   }
 
   const [base, quote] = pair.split(':');
   if (!base || !quote) {
-    return res.status(400).json({ success: false, data: [] });
+    return res.status(400).json({ success: false, meta: {}, data: [] });
   }
 
-  const candles = await FtxRepository.getCandles({ base, quote });
+  let candles = [];
+  switch (exchange) {
+    case Exchange.FTX:
+      candles = await FtxRepository.getCandles({ base, quote });
+      break;
+    case Exchange.Binance:
+      candles = await BinanceRepository.getCandles({ base, quote });
+      break;
+  }
 
   if (candles.length === 0) {
     return res.status(404).json({
       success: false,
+      meta: { exchange },
       data: [],
     });
   }
@@ -26,6 +44,7 @@ export default async (req: Request, res: Response) => {
   res.setHeader('Cache-Control', 'public, max-age=30');
   return res.status(200).json({
     success: true,
+    meta: { exchange },
     data: candles,
   });
 };
---
+++ src/domain/exchanges/index.ts
@@ -0,0 +1,4 @@
+export enum Exchange {
+  Binance = 'binance',
+  FTX = 'ftx',
+}
