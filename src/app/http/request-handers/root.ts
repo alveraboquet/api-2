675c5ada6de78e2dc86e17ad0aa41e859495c5ff
jinxcapital <jinxcapital@protonmail.com>
2021-12-04 13:03:08 +0100

    :art: Simplify API docs

--- src/app/http/request-handers/root.ts
+++ src/app/http/request-handers/root.ts
@@ -1,50 +1,23 @@
-import { Exchange } from 'domain/exchanges';
 import { Request, Response } from 'express';
-
-const SUPPORTED_EXCHANGES = [
-  Exchange.Binance,
-  Exchange.Coinbase,
-  Exchange.FTX,
-  Exchange.Kucoin,
-];
-
-const SUPPORTED_RESOLUTIONS = [900, 3600, 14400, 21600, 86400];
 
 export default (_req: Request, res: Response) => {
   res.setHeader('Cache-Control', 'public, max-age=30');
   res.status(200).json([
-    {
-      route: '/candles/:pair',
-      params: {
-        pair: {
-          type: 'string',
-          required: true,
-        },
-        exchange: {
-          type: 'string',
-          required: false,
-          default: 'ftx',
-          values: SUPPORTED_EXCHANGES,
-        },
-        limit: {
-          type: 'number',
-          required: false,
-          default: 48,
-        },
-        resolution: {
-          type: 'number',
-          required: false,
-          default: 900,
-          values: SUPPORTED_RESOLUTIONS,
-        },
-      },
-      examples: [
-        '/candles/btc:usd',
-        '/candles/eth:usdc?exchange=coinbase',
-        '/candles/bnb:busd?exchange=binance&limit=3',
-        '/candles/trx:usdt?exchange=kucoin&limit=3&resolution=3600',
-      ],
-      note: 'Coinbase does not support the 14400 resolution and FTX does not support the 21600 resolution.',
-    },
+    ['/candles/btc:usd', '/candles/eth:usd', '/candles/ltc:usd'],
+    [
+      '/candles/eth:usdc?exchange=coinbase',
+      '/candles/bnb:busd?exchange=binance',
+      '/candles/ftt:usdt?exchange=ftx',
+      '/candles/kcs:usdt?exchange=kucoin',
+    ],
+    [
+      '/candles/btc:usdt?exchange=coinbase&limit=1',
+      '/candles/btc:usdt?exchange=coinbase&limit=2',
+      '/candles/btc:usdt?exchange=coinbase&limit=3',
+      '/candles/eth:usdt?exchange=ftx&limit=3&resolution=900',
+      '/candles/eth:usdt?exchange=ftx&limit=3&resolution=3600',
+      '/candles/eth:usdt?exchange=ftx&limit=3&resolution=14400',
+      '/candles/eth:usdt?exchange=ftx&limit=3&resolution=86400',
+    ],
   ]);
 };
