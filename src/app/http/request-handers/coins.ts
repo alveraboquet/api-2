56fa0d832121e4cb8f3ee9cf9fc157c2a92376df
jinxcapital <jinxcapital@protonmail.com>
2021-12-05 16:52:58 +0100

    :sparkles: Basic filter

--- src/app/http/request-handers/coins.ts
+++ src/app/http/request-handers/coins.ts
@@ -1,7 +1,7 @@
 import CoingeckoCoinsRepository from 'app/coins/coingecko-repository';
 import { Request, Response } from 'express';
 
-export default async (_req: Request, res: Response) => {
+export default async (req: Request, res: Response) => {
   res.setHeader('Cache-Control', 'public, max-age=0');
 
   const coins = await CoingeckoCoinsRepository.getCoins();
@@ -13,14 +13,28 @@ export default async (_req: Request, res: Response) => {
     });
   }
 
+  const filter = req.query.filter as string;
+  const meta: { filter?: string } = {};
+  if (filter) {
+    meta.filter = filter;
+  }
+
+  let data = coins.map((coin) => ({
+    id: coin.id,
+    symbol: coin.symbol,
+    name: coin.name,
+  }));
+  if (filter) {
+    data = data.filter(
+      (coin) =>
+        coin.symbol === filter || coin.name === filter || coin.id === filter,
+    );
+  }
+
   res.setHeader('Cache-Control', 'public, max-age=60');
   return res.status(200).json({
     success: true,
-    meta: {},
-    data: coins.map((coin) => ({
-      id: coin.id,
-      symbol: coin.symbol,
-      name: coin.name,
-    })),
+    meta,
+    data,
   });
 };
