b78248ced93a3e4f8c4d318abf0190714c3031a0
jinxcapital <jinxcapital@protonmail.com>
2021-12-05 12:56:21 +0100

    :sparkles: Example coin detail endpoint

--- src/app/http/request-handers/root.ts
+++ src/app/http/request-handers/root.ts
@@ -2,26 +2,24 @@ import { Request, Response } from 'express';
 
 export default (_req: Request, res: Response) => {
   res.setHeader('Cache-Control', 'public, max-age=30');
-  res
-    .status(200)
-    .json([
-      ['/chart/btc:usd.jpg', '/chart/eth:usd.jpg', '/chart/ltc:usd.jpg'],
-      ['/coins'],
-      ['/candles/btc:usd', '/candles/eth:usd', '/candles/ltc:usd'],
-      [
-        '/candles/eth:usdc?exchange=coinbase',
-        '/candles/bnb:busd?exchange=binance',
-        '/candles/ftt:usdt?exchange=ftx',
-        '/candles/kcs:usdt?exchange=kucoin',
-      ],
-      [
-        '/candles/btc:usdt?exchange=coinbase&limit=1',
-        '/candles/btc:usdt?exchange=coinbase&limit=2',
-        '/candles/btc:usdt?exchange=coinbase&limit=3',
-        '/candles/eth:usdt?exchange=ftx&limit=3&resolution=900',
-        '/candles/eth:usdt?exchange=ftx&limit=3&resolution=3600',
-        '/candles/eth:usdt?exchange=ftx&limit=3&resolution=14400',
-        '/candles/eth:usdt?exchange=ftx&limit=3&resolution=86400',
-      ],
-    ]);
+  res.status(200).json([
+    ['/chart/btc:usd.jpg', '/chart/eth:usd.jpg', '/chart/ltc:usd.jpg'],
+    ['/coins', '/coins/btc', '/coins/bitcoin', '/coins/eth', '/coins/ethereum'],
+    ['/candles/btc:usd', '/candles/eth:usd', '/candles/ltc:usd'],
+    [
+      '/candles/eth:usdc?exchange=coinbase',
+      '/candles/bnb:busd?exchange=binance',
+      '/candles/ftt:usdt?exchange=ftx',
+      '/candles/kcs:usdt?exchange=kucoin',
+    ],
+    [
+      '/candles/btc:usdt?exchange=coinbase&limit=1',
+      '/candles/btc:usdt?exchange=coinbase&limit=2',
+      '/candles/btc:usdt?exchange=coinbase&limit=3',
+      '/candles/eth:usdt?exchange=ftx&limit=3&resolution=900',
+      '/candles/eth:usdt?exchange=ftx&limit=3&resolution=3600',
+      '/candles/eth:usdt?exchange=ftx&limit=3&resolution=14400',
+      '/candles/eth:usdt?exchange=ftx&limit=3&resolution=86400',
+    ],
+  ]);
 };
