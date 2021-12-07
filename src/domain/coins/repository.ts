3531198f1b0abe20769a94d6c6a2fa9961e3fb07
jinxcapital <jinxcapital@protonmail.com>
2021-12-07 21:08:45 +0100

    :label: Type coins[]

--- src/domain/coins/repository.ts
+++ src/domain/coins/repository.ts
@@ -1,6 +1,7 @@
 import { Coin } from './index';
 
 export interface CoinRepository {
+  coins: Coin[];
   getCoins(): Promise<Coin[]>;
   getCoinsTop(): Promise<Coin[]>;
   find(q: string): Promise<Coin | null>;
